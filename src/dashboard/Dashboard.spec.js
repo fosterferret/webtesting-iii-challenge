a66582110d229545be88c7044bd67fe4a9887b6d
Francis Bulus <notchera@gmail.com>
2019-10-14 21:43:17 +0100

    added gate tests - I had completely missed them the first time

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Controls from "../controls/Controls";
 import Display from "../display/Display";
+import Dashboard from './Dashboard';
 
 // Test away
 test("Dashboard shows controls", () => {
@@ -11,3 +12,19 @@ test("Dashboard shows controls", () => {
 test("Dashboard shows display", () => {
   render(<Display />);
 });
+
+
+// gate defaults to unlocked and open
+test('gate defaults to unlocked and open', () => {
+    const dashboard = render(<Dashboard />);
+    expect(dashboard.getByText(/open/i));
+    expect(dashboard.getByText(/unlocked/i));
+  });
+  
+  // gate cannot be closed or opened if it is locked
+  test('gate cannot be closed or opened if it is locked', () => {
+    const component = render(<Controls locked={true} closed={true} />);
+    expect(component.queryByText(/close gate/i)).toBe(null);
+    const openButton = component.getByText(/open gate/i);
+    expect(openButton.disabled).toBe(true);
+  });
