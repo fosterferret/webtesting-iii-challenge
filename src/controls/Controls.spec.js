4d66f534c3f19b753593b5c1527f757d5bd2ff43
Francis Bulus <notchera@gmail.com>
2019-10-14 07:48:45 +0100

    the last two tests from the MVP requirements for the controls passed

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -13,15 +13,21 @@ test('The component provides buttons to toggle closed and locked states', () => 
 //- buttons' text changes to reflect the state the door will be in if clicked
 test('Button text changes to reflect reflect the state the door will be in if clicked', () => {
     const controls = render(<Controls locked={true} />);
-  
     const unlockGate = controls.getByText(/unlock gate/i);
     fireEvent.click(unlockGate);
     controls.findByText(/unlocked/i);
   });
 
-
+// - the closed toggle button is disabled if the gate is locked
   test('The closed button of the component is disabled if the gate is locked', () => {
     const component = render(<Controls locked={true} closed={true} />);
     const button = component.getByText(/open gate/i);
+    expect(button.disabled).toBe(true);
+  });
+
+// - the locked toggle button is disabled if the gate is open
+test('The locked button of the component is disabled if the gate is open', () => {
+    const component = render(<Controls locked={false} closed={false} />);
+    const button = component.getByText(/lock gate/i);
     expect(button.disabled).toBe(true);
   });
